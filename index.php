dbd79b065ef40c08175bcab7a5c2b591acd2b060
Vanya <kolotov2001@bk.ru>
2024-07-21 02:03:22 +0400

    Some code

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+namespace main;
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>METANIT.COM</title>
+<meta charset="utf-8" />
+</head>
+<body>
+<h1>Сайт на PHP</h1>
+<?php
+echo "1122";
+?>
+</body>
+</html>
